5350ca92d962d15b604201f3169166ed4a136c8e
rolo <rolo.afrole@gmail.com>
2020-05-02 16:01:15 +0900

    update

--- sim_main.py
+++ sim_main.py
@@ -35,7 +35,28 @@ def sim(argv):
         basename = './data/base_30day'
         # basename = './data/202003010352_wn'
         # basename = './data/202003010555_wc'
-        plot(basename)
+        names = []
+        # names.append('./data/30d_002p')
+        # names.append('./data/30d_0018p')
+        names.append(('./data/30-60_0h',
+                      ['0 hour in crowded zone',
+                       '0 hour in crowded zone'], ['o', 'o']))
+        names.append(('./data/30-60_1h',
+                      ['1 hour in crowded zone',
+                       '1 hour in crowded zone'], ['v', 'v']))
+        names.append(('./data/30-60_2h',
+                      ['2 hour in crowded zone',
+                       '2 hour in crowded zone'], ['s', 's']))
+        names.append(('./data/30-60_4h',
+                      ['4 hour in crowded zone',
+                       '4 hour in crowded zone'], ['+', '+']))
+        names.append(('./data/30-60_6h',
+                      ['6 hour in crowded zone',
+                       '6 hour in crowded zone'], ['x', 'x']))
+        names.append(('./data/30-60_8h',
+                      ['8 hour in crowded zone',
+                       '8 hour in crowded zone'], ['1', '1']))
+        plots(names)
 
 
 def calc(config=FLAGS):
@@ -50,8 +71,22 @@ def calc(config=FLAGS):
 
 def plot(basename):
     status = util.load_status(basename)
-    util.plot_sim(status, basename, title=None)
-    util.save_as_csv(status, basename)
+    util.plot_sim(status, basename, title=None, ylimit=[0, 2000])
+    # util.save_as_csv(status, basename)
+
+
+def plots(names):
+    items = []
+    for item in names:
+        status = util.load_status(item[0])
+        items.append((status, item[1], item[2]))
+    util.plot_sims(items,
+                   infected=True,
+                   segregated=False,
+                   filename=None,
+                   title=None,
+                   ylimit=[0, 2000],
+                   xlimit=[25, 62])
 
 
 if __name__ == '__main__':
--- util.py
+++ util.py
@@ -5,8 +5,9 @@ import pickle
 from datetime import datetime
 
 
-def plot_sim(status, basename=None, save=True, title=None):
+def plot_sim(status, basename=None, save=True, title=None, ylimit=None):
 
+    # plt.figure()
     infl_array = np.asarray(status.h_infl)
     seg_array = np.asarray(status.h_seg)
     x_array = np.arange(infl_array.size)
@@ -16,6 +17,9 @@ def plot_sim(status, basename=None, save=True, title=None):
 
     if title is not None:
         plt.title(title)
+
+    if ylimit is not None:
+        plt.ylim(ylimit)
 
     plt.xlabel('Day')
     plt.ylabel('Population')
@@ -62,3 +66,52 @@ def save_as_csv(status, basename):
                delimiter=',',
                fmt='%d',
                header='infl,segregated')
+
+
+def plot_sims(status_list,
+              infected=True,
+              segregated=True,
+              filename=None,
+              save=True,
+              title=None,
+              ylimit=None,
+              xlimit=None):
+
+    # plt.figure()
+
+    for status, labels, marks in status_list:
+        infl_array = np.asarray(status.h_infl)
+        seg_array = np.asarray(status.h_seg)
+        x_array = np.arange(infl_array.size)
+        if infected:
+            plt.plot(x_array, infl_array, label=labels[0], marker=marks[0])
+        if segregated:
+            plt.plot(x_array, seg_array, label=labels[1], marker=marks[1])
+
+    plt.legend()
+
+    if title is not None:
+        plt.title(title)
+
+    if ylimit is not None:
+        plt.ylim(ylimit)
+
+    if xlimit is not None:
+        plt.xlim(xlimit)
+
+    plt.xlabel('Day')
+    plt.ylabel('Population')
+    if infected and not segregated:
+        plt.ylabel('Infected Population')
+    if not infected and segregated:
+        plt.ylabel('Removed Population')
+
+    if filename is None:
+        filename = datetime.now().strftime('%Y%m%d%H%M')
+
+    filename = '{}.png'.format(filename)
+
+    if save:
+        plt.savefig(filename, bbox_inches='tight', pad_inches=0.0)
+    else:
+        plt.show()
